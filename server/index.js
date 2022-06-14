ec5416f5c26bf941f68ac35acecb63aa09bec8d2
onkr0d <90716666+onkr0d@users.noreply.github.com>
2022-06-14 19:07:49 -0400

    refactor into mono repo, impl server, connect to web

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require('path');
+const fs = require("fs");
+const cors = require("cors");
+const port = 8080;
+
+const app = express();
+
+app.use(cors(), express.static(path.join(__dirname, 'clubs')))
+
+app.get("/api/clubs", cors(), (req, res, next) => {
+    const directoryPath = path.join(__dirname, 'clubs');
+
+    fs.readdir(directoryPath, function (err, files) {
+        if (err) {
+            console.log("Cannot read directory", err)
+        }
+        return res.json(JSON.stringify(files));
+    })
+})
+
+app.listen(port, () => {
+    console.log('server listening on', port);
+})
