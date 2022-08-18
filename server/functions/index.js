b9c90376396072b1264bd9492c13c8a18017cd3a
onkr0d <90716666+onkr0d@users.noreply.github.com>
2022-08-18 17:10:34 -0400

    slight clean up

--- server/functions/index.js
+++ server/functions/index.js
@@ -9,10 +9,10 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json())
 
-
 app.get("*", (request, response) => {
     response.send("Hello from Express on Firebase!")
     functions.logger.info(request.body);
-    // parse json from request body
     functions.logger.info(request.body.name);
+    functions.logger.info("User '" + request.body.name + "' with SUid " + request.body.id + " is trying to sign into " + request.body.clubName);
+
 })
