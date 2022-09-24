591a9c2dab1dc0b2d978d0904c0dbee6263fbdc7
onkr0d <90716666+onkr0d@users.noreply.github.com>
2022-09-23 20:04:41 -0400

    add loading overlay to clubs when sign in
    should help prevent multiple sign in attempts in the same second. not a conclusive solution to #25, however.

--- server/functions/index.js
+++ server/functions/index.js
@@ -117,3 +117,7 @@ app.get("/api/clubs", cors(), async (request, response) => {
 
     response.status(200).send(JSON.stringify(final));
 })
+
+app.post("/api/fakeupdate", cors(), async (request, response) => {
+    response.status(200).send(JSON.stringify("👍"));
+})
--- web/src/components/SingleClub.js
+++ web/src/components/SingleClub.js
@@ -2,73 +2,78 @@ import React, {useState} from "react";
 import ky from 'ky';
 import {useCookies} from "react-cookie";
 import toast, {Toaster} from 'react-hot-toast';
+import LoadingOverlay from 'react-loading-overlay-ts';
 
 /**
  * This class represents a single club.
  */
 function SingleClub(props) {
-    useState({
-        clubName: props.clubName,
-        description: props.description,
-        imageSource: props.imageSource,
-        activated: props.activated
-    });
-
     const [cookies] = useCookies(['name', 'suffolkID']);
+    let [inProgress, setInProgress] = useState(false);
 
     async function submit() {
-        if (!props.activated) {
-            toast.error('This club is not currently activated. Contact club leaders for more information.')
-
-            // I wanna see who tries to sign up :>
-            // this is an endpoint just to log who tries to sign up for a club that isn't activated
-            ky.post("https://us-central1-suvba-354520.cloudfunctions.net/app/api/signindemo/club", {
-                json: {
-                    name: cookies.name, id: cookies.suffolkID, clubName: props.clubName
-                }
-            })
-            return;
-        }
+        // if (!props.activated) {
+        //     toast.error('This club is not currently activated. Contact club leaders for more information.')
+        //
+        //     // I wanna see who tries to sign up :>
+        //     // this is an endpoint just to log who tries to sign up for a club that isn't activated
+        //     ky.post("https://us-central1-suvba-354520.cloudfunctions.net/app/api/signindemo/club", {
+        //         json: {
+        //             name: cookies.name, id: cookies.suffolkID, clubName: props.clubName
+        //         }
+        //     })
+        //     return;
+        // }
 
         // https://expressjs.com/en/guide/writing-middleware.html
         // according to the above we can send cookies as part of the
         // request explicitly, no need to put them in a body
         // unfortunately this won't make it into the MVP LOL
-
+        setInProgress(true)
         // is not having internet an edge case? how did user even get to this situation??
-        let response;
         try {
             // this is a prod endpoint
-            response = await ky.post("https://us-central1-suvba-354520.cloudfunctions.net/app/api/update", {
+            // i wonder if we need some more error handling here.
+            await ky.post("http://localhost:5001/suvba-354520/us-central1/app/api/fakeupdate", {
                 json: {
                     name: cookies.name, id: cookies.suffolkID, clubName: props.clubName.toLowerCase().replace(" ", "-")
                 }
             }).json()
         } catch (e) {
             console.error("Something went wrong!")
+            toast.error('something went wrong! please try again later.')
             return;
         }
 
-        console.log(response)
         // whoops haha there's no signed in screen :)
-        toast.success("Signed in successfully! You may now close this tab.")
+        toast.success("signed in successfully! you may now close this tab.", {
+            style: {textAlign: 'center',}
+        });
+        // this is deprecated :C
+        setInProgress(false);
     }
 
-    return <button type={"submit"} onClick={submit}
-                   className="hover:bg-gray-200 max-w-md mx-auto bg-white rounded-xl border shadow-md overflow-hidden md:max-w-2xl dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-        <div className="md:flex">
-            <div className="md:shrink-0">
-                <img className="h-48 w-full object-cover md:h-full md:w-48 object-scale-down" src={props.imageSource}
-                     alt="Club Icon"/>
-            </div>
-            <div className="p-8">
-                <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{props.clubName}</div>
-                <div className={"py-[1%]"}/>
-                <p className="text-black font-normal dark:text-gray-400">{props.description}</p>
-            </div>
-        </div>
+    return <div>
         <Toaster/>
-    </button>
+        <LoadingOverlay active={inProgress} spinner text='signing in...' className={"rounded-xl"}>
+            <button type={"submit"} onClick={submit}
+                    className="hover:bg-gray-200 max-w-md mx-auto bg-white rounded-xl border shadow-md overflow-hidden md:max-w-2xl dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <div className="md:flex">
+                    <div className="md:shrink-0">
+                        <img className="h-48 w-full object-cover md:h-full md:w-48 object-scale-down"
+                             src={props.imageSource}
+                             alt="Club Icon"/>
+                    </div>
+                    <div className="p-8">
+                        <div
+                            className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{props.clubName}</div>
+                        <div className={"py-[1%]"}/>
+                        <p className="text-black font-normal dark:text-gray-400">{props.description}</p>
+                    </div>
+                </div>
+            </button>
+        </LoadingOverlay>
+    </div>
 }
 
 export default SingleClub;
